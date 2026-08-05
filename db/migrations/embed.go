a7502e2a7a43546eb23983bee61d00d51424b8a6
Au1rxx <au1rxx@users.noreply.github.com>
2026-08-05 13:05:22 +0000

    chore: keep public repository artifact-only

--- db/migrations/embed.go
+++
@@ -1,9 +0,0 @@
-// Package migrations embeds the versioned MySQL schema files.
-package migrations
-
-import "embed"
-
-// Files contains this directory. The migration loader selects only *.sql.
-//
-//go:embed *.sql
-var Files embed.FS
